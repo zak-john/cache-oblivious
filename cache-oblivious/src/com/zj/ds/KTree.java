14307dc1ee483b96fdc98fd979c0555d1c11d345
Zak John <zakzakjohn@gmail.com>
2019-07-04 15:19:27 +0100

    Basic funnelsort algorithm setup done, collection based

---
+++ cache-oblivious/src/com/zj/ds/KTree.java
@@ -0,0 +1,10 @@
+package com.zj.ds;
+
+import java.util.List;
+
+public interface KTree {
+
+    void init(List subArrays);
+
+    List sort();
+}
---
+++ cache-oblivious/src/com/zj/ds/KTreeImpl.java
@@ -0,0 +1,17 @@
+package com.zj.ds;
+
+import java.util.List;
+
+public class KTreeImpl implements KTree {
+    private List subArrays;
+
+    @Override
+    public void init(List subArrays) {
+        this.subArrays = subArrays;
+    }
+
+    @Override
+    public List sort() {
+        return null;
+    }
+}
